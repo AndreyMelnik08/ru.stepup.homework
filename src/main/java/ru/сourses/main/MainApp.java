71f82cbd6d342fa09ded0c16498ad226449d4d64
Andrey Melnik <andreum.melnik@yandex.ru>
2024-08-15 16:32:03 +0300

    Класс object #3. Сравнение линий

--- src/main/java/ru/сourses/main/MainApp.java
+++ src/main/java/ru/сourses/main/MainApp.java
@@ -1,14 +1,15 @@
 package ru.сourses.main;
 
+import ru.сourses.geometry.Line;
 import ru.сourses.geometry.Point;
 
 public class MainApp {
 
     public static void main(String[] args) throws CloneNotSupportedException {
 
-        Point point1 = new Point(3, 4);
-        Point point2 = point1.clone();
-        System.out.println(point1.equals(point2));
+        Line line1 = new Line(new Point(5,6), new Point(8,9));
+        Line line2 = line1.clone();
+        System.out.println(line1.equals(line2));
 
     }
 }
