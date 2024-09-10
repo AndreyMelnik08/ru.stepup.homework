b44bd75dcf07f1943ea7ff1f45bbc8eb33a4a69a
Andrey Melnik <andreum.melnik@yandex.ru>
2024-09-11 00:48:14 +0300

    Обнуление полей

--- src/main/java/ru/сourses/main/MainApp.java
+++ src/main/java/ru/сourses/main/MainApp.java
@@ -1,17 +1,37 @@
 package ru.сourses.main;
 
-import ru.stepup.homework.Sauce;
-import ru.stepup.homework.Sharpness;
-import ru.сourses.geometry.Line;
-import ru.сourses.geometry.Point;
-import ru.сourses.geometry.PolyLine;
+import ru.stepup.homework.Cat;
+import ru.stepup.homework.Dog;
+
+import java.lang.reflect.Field;
+import java.util.*;
 
 public class MainApp {
 
-    public static void main(String[] args) throws CloneNotSupportedException {
+    public static void main(String[] args) throws IllegalAccessException {
+        Cat cat;
+        Dog dog;
+        cat = new Cat("Pushok", 3, new ArrayList<>(Arrays.asList("Sharik", "Ryan Gosling", "Cristiano Ronaldo")));
+        dog = new Dog("Sharik", 5, new ArrayList<>(Arrays.asList("Pushok", "Kim Kardashian", "Justin Bieber")));
+        System.out.println("До обнуления: " + cat);
+        nullifyFields(cat);
+        System.out.println("После обнуления: " + cat);
+        System.out.println("До обнуления: " + dog);
+        nullifyFields(dog);
+        System.out.println("После обнуления: " + dog);
+    }
 
-        Sauce sauce = new Sauce("Chili", Sharpness.VERY_SPICY);
-        System.out.println(sauce);
-
+    public static void nullifyFields(Object obj) throws IllegalAccessException {
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.getType().isPrimitive()) {
+                if (field.getType() == int.class) {
+                    field.setInt(obj, 0);
+                }
+            } else {
+                field.set(obj, null);
+            }
+        }
     }
 }
